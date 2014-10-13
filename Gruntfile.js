e37c41df10d0115ddcddcb078af47e97e2feb2fe
Korbinian Polk <korbinianpolk@googlemail.com>
2014-10-13 16:05:21 +0200

    added gitignore

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,7 +1,31 @@
 module.exports = function(grunt) {
 
-  grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json')
-  });
+
+
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+
+        'ftp-deploy': {
+            build: {
+                auth: {
+                    host: 'korbinianpolk.de',
+                    port: 21,
+                    authKey: 'key1'
+                },
+                src: 'build',
+                dest: '',
+                exclusions: []
+            }
+        }
+
+    });
+
+
+  
+
+    grunt.loadNpmTasks('grunt-ftp-deploy');
+
+    grunt.registerTask('default', ['ftp-deploy']);
+
 
 };
